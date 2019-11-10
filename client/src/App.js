b8c82c8db5dbdacfa49df46fae4b62d598b47c08
Jose Medrano (Joe) <Joselmedrano@gmail.com>
2019-11-10 09:29:58 +0000

    -Added a login form.
    -Added axios post for login form.
    
    -A verification of is needed before proceeding to the next step.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import Login from "./components/Login";
+import Login from "./components/Login"
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
-      <div className="App">
+        <div className="App">
+        {/*Links*/}
+            <nav>
+                <div><Link to="/">Home</Link></div>
+                <div><Link to="/login">Log-in</Link></div>
+            </nav>
+
+        {/*Routes*/}
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
+        </div>
     </Router>
   );
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios"
+const Login = (props) => {
+	const [data, setData] = useState({
+		username: "",
+		password: "",
+	})
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+	const handleChange = (event) => {
+		setData({
+			...data,
+			[event.target.name]: event.target.value,
+		})
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+        axios
+        .post(`http://localhost:5000/api/login`, data)
+        .then(response => {
+            console.log(data)
+        localStorage.setItem("token", response.data.payload)
+			})
+	    .catch(error => {
+                console.log(error);
+			})
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+
+			<input type="text" name="username" placeholder="Username" value={data.username} onChange={handleChange} />
+			<input type="password" name="password" placeholder="Password" value={data.password} onChange={handleChange} />
+
+			<button type="submit">Log-in</button>
+		</form>
+	)
 };
 
 export default Login;
