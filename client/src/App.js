d2794601ef0764a372f3ccd9535d6ce08dbc68ce
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2019-11-11 20:35:38 -0600

    -Added protected routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import ProtectedRoute from "./components/ProtectedRoute"
 import Login from "./components/Login"
+import BubblePage from "./components/BubblePage"
 import "./styles.scss";
 
 function App() {
@@ -12,10 +13,12 @@ function App() {
             <nav>
                 <div><Link to="/">Home</Link></div>
                 <div><Link to="/login">Log-in</Link></div>
+                 <div><Link to="/account">Account</Link></div>
             </nav>
 
         {/*Routes*/}
         <Route exact path="/" component={Login} />
+        <ProtectedRoute exact path="/account" component={BubblePage}/>
         </div>
     </Router>
   );
