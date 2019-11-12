7f2becbb41082faaf58a9d373f4863078deb6a3c
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2019-11-11 20:59:41 -0600

    Added get function to bubble page.
    Needs confirmation of get data.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import api from '../utils/api';
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const [user, setUser] = useState({
+		username: "",
+	})
+
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  useEffect(() => {
+		api().get("/api/colors")
+			.then(response => {
+        console.log(`api color response ${Response}`)
+        setColorList(response)
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}, [])
   return (
     <>
+    
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
